82bd6d77a3bfafe8b96da86a4e4db125b0c93b6f
DanielDeshmukh <deshmukhdaniel2005@gmail.com>
2026-08-22 12:09:24 +0530

    chore: remove scripts from git tracking (gitignored, contain secrets)

--- scripts/list-brands.js
+++
@@ -1,6 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const p = new PrismaClient();
-p.brand.findMany({ select: { id: true, name: true, _count: { select: { products: true } } } })
-  .then(function(r) { console.log(JSON.stringify(r, null, 2)); })
-  .catch(function(e) { console.error(e); })
-  .then(function() { p.$disconnect(); });
